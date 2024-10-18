25299866153448906b0104f1781c240fbb26d92d
JasmineRowe <jasrowe@hotmail.com>
2024-10-18 22:58:58 +0100

    Final edits before deployment

--- Assets/script.js
+++ Assets/script.js
@@ -1,43 +1,99 @@
+let tasks = [];
+let completedtasks = [];
 
+document.addEventListener("DOMContentLoaded", () => {
+  const storedTasks = JSON.parse(localStorage.getItem('tasks'))
+  if (storedTasks){
+    storedTasks.forEach((task) => tasks.push(task))
+    updateTaskList();
+    updateCompletedTaskList();
+  };
+});
 
+const saveTasks = () => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+    localStorage.setItem("completedtasks", JSON.stringify(completedtasks))
+};
 
+const addTask = () => {
+  const taskInput = document.getElementById("taskInput");
+  const taskVal = taskInput.value.trim();
 
+  if (taskVal) {
+    tasks.push({text:taskVal});
+    taskInput.value = "";
+    updateTaskList();
+    saveTasks();
+  };
+};
 
-const taskElement = document.getElementById("addTask");
+const updateCompletedTaskList = () => {
+  const completedtaskListTable = document.getElementById("completedtasktable");
+    completedtaskListTable.innerHTML = "";
+    completedtasks.forEach((completedtask, index) => {
+      const newCompletedTaskRow = completedtaskListTable.insertRow(-1);
+        newCompletedTaskRow.innerHTML =  `
+            <tr>
+              <td>${completedtask.text}</td>
+              <td><button class="deleteButton" id="deleteButton" onclick="deletecompletedItem(${index})">Delete Item </button></td>
+            </tr>
+        `;
+        completedtaskListTable.appendChild(newCompletedTaskRow);
+    });
+};
 
-const taskButtonElement = document.getElementById("add-task-btn");
+const deletecompletedItem = (index) => {
+  completedtasks.splice(index,1);
+  updateCompletedTaskList();  
+}
 
+const deleteItem = (index) => {
+  tasks.splice(index,1);
+  updateTaskList();  
+};
+  
+const editItem = (index) => {
+  const taskInput = document.getElementById("taskInput")
+  taskInput.value = tasks[index].text
+  tasks.splice(index,1);
+  updateTaskList();
+};
+  
+const moveToCompleted = (index) => {
+  completedtasks.push(tasks[index]); 
+  tasks.splice(index,1);
+  updateTaskList();
+  updateCompletedTaskList();
+};
+  
+const updateTaskList = () => {
+  const taskListTable = document.getElementById("taskList");
+  taskListTable.innerHTML = "";
+  tasks.forEach((task, index) => {
+    const newTaskRow = taskListTable.insertRow(-1);
+    newTaskRow.innerHTML =  `     
+        <td>${task.text}</td>
+        <td><button class="complete-btn" id="completeButton" onclick="moveToCompleted(${index})">Mark as Complete </button>
+            <button class="edit-btn" id="editButton" onclick="editItem(${index})">Edit Item </button>
+            <button class="deleteButton" id="deleteButton" onclick="deleteItem(${index})">Delete Item </button>
+        </td>
+`;
+  taskListTable.appendChild(newTaskRow);
+  });
+};
 
-const button = document.createElement('button');
-
-button.innerText = 'Completed';
-
-button.id = 'mainButton';
-
-const taskVal = taskElement.value;
-
-taskButtonElement.addEventListener("click", function() {
-
-const taskVal = taskElement.value;
-
- // Get a reference to the table
-
- const taskListTable = document.getElementById("taskTable");
-
- // Insert a row at the end of the table
-
- const newTaskRowEl = taskListTable.insertRow(-1);
-
-   // Insert a cell in the row at index 0
-
- const newTaskCell = newTaskRowEl.insertCell(0);
- 
- newTaskCell.innerHTML = taskVal;
-
- const newTaskCell1 = newTaskRowEl.insertCell(1);
-
- newTaskCell1.appendChild(button);
-
+document.getElementById("add-task-btn").addEventListener("click", function (e){
+  e.preventDefault();
+  addTask ();
 })
+  
+ 
+ 
+
+
+
+
+
+
 
 
